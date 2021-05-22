720440460e44c5a04eea2e9a9494fe7a4a94e7f2
learnchi <learnchi@hacka.work>
2021-05-22 16:42:29 +0900

    dataの持ち方を変更。v-bind:classがうまくいっていない。

--- js/sudoku.js
+++ js/sudoku.js
@@ -3,24 +3,33 @@
  */
 'use strict';
 
-//tableを初期化
-let initializeData = () => {
-	// 初期化したブロック
-	let blankBlock = { disp: '-', onmouse: false, samenumber: false, relatedblock: false};
-
-	let colsArr = [];
-	for (let i = 0; i < 9; i++) {
-		colsArr.push(JSON.parse(JSON.stringify(blankBlock)));
-	}
-
-	let rowsArr = [];
-	for (let j = 0; j < 9; j++) {
-		rowsArr.push({cols: JSON.parse(JSON.stringify(colsArr))});
-	}
-	let rows = {rows: rowsArr};
-	return rows;
-};
-let data = initializeData();
+// 表示
+let disp=['-','-','-','-','-','-','-','-','-',
+	'-','-','-','-','-','-','-','-','-',
+	'-','-','-','-','-','-','-','-','-',
+	'-','-','-','-','-','-','-','-','-',
+	'-','-','-','-','-','-','-','-','-',
+	'-','-','-','-','-','-','-','-','-',
+	'-','-','-','-','-','-','-','-','-',
+	'-','-','-','-','-','-','-','-','-',
+	'-','-','-','-','-','-','-','-','-',
+];
+// マウスオーバー時のスタイルの適用
+let isOnmouse=[false,false,false,false,false,false,false,false,false,
+	false,false,false,false,false,false,false,false,false,
+	false,false,false,false,false,false,false,false,false,
+	false,false,false,false,false,false,false,false,false,
+	false,false,false,false,false,false,false,false,false,
+	false,false,false,false,false,false,false,false,false,
+	false,false,false,false,false,false,false,false,false,
+	false,false,false,false,false,false,false,false,false,
+	false,false,false,false,false,false,false,false,false,
+];
+// 同一番号時のスタイルの適用
+let isSamenumber = isOnmouse.slice();
+// 同一ブロック時のスタイルの適用
+let isRelatedblock = isOnmouse.slice();
+;
 
 // 埋める順番を決める
 let order=[81,74,78,18,11,15,54,47,51,
@@ -55,17 +64,18 @@ let makeSudoku = function(od) {
 	// チェック対象配列を作成
 	let chkArray = [];
 	// 縦
-	data.rows.forEach((item, index)=> {
-		if (index !== r) {
-			chkArray.push(item.cols[c].disp);
+	for (let i=0; i<9; i++) {
+		if (i !== r) {
+			chkArray.push(disp[i*9+c]);
 		}
-	});
+	}
 	// 横
-	data.rows[r].cols.forEach((item, index)=> {
-		if (index !== c) {
-			chkArray.push(item.disp);
+	for (let i = 0; i<9; i++) {
+		if (i !== c) {
+			chkArray.push(disp[r*9+i]);
 		}
-	});
+	}
+
 	// ブロック
 	let currGroupRow = Math.floor(r/3); // 1～3行目は0、4～6行目は1、7～9行目は2
 	let currGroupCol = Math.floor(c/3); // 1～3列目は0、4～6列目は1、7～9列目は2
@@ -73,14 +83,15 @@ let makeSudoku = function(od) {
 	for (let gr = currGroupRow*3; gr < currGroupRow*3+3; gr++) {
 		for (let gc = currGroupCol*3; gc < currGroupCol*3+3; gc++) {
 			if (gr !== r && gc != c) {
-				chkArray.push(data.rows[gr].cols[gc].disp);
+				chkArray.push(disp[gr*9+gc]);
 			}
 		}
 	}
 
 	// 1～9の候補を探す
 	let khArray = [1,2,3,4,5,6,7,8,9];
-	khArray.sort(() => Math.random() - 0.5);
+	khArray.sort(() => Math.random() - 0.5);// ランダムな配列にする
+
 	let khArrayIdx = 0;
 	do {
 		if (khArrayIdx++ >= 9) {
@@ -89,15 +100,15 @@ let makeSudoku = function(od) {
 		}
 	// チェック対象配列内、自分自身、履歴に同じ数字があったら次の数字
 	} while (chkArray.indexOf(khArray[khArrayIdx]) > -1 ||
-			data.rows[r].cols[c].disp == khArray[khArrayIdx] ||
+			disp[odIdx] == khArray[khArrayIdx] ||
 			historyArray[odIdx].indexOf(khArray[khArrayIdx]) > -1)
 
 	if (khArrayIdx < 9) {// 候補がある場合は表示と履歴記録をして次のセルに進む
-		data.rows[r].cols[c].disp=khArray[khArrayIdx];
+		disp[odIdx]=khArray[khArrayIdx];
 		historyArray[odIdx].push(khArray[khArrayIdx]);
 		od++;
 	} else { // 候補がない場合は自分自身の表示と履歴をリセットして前のセルをやり直す
-		data.rows[r].cols[c].disp="-";
+		disp[odIdx]="-";
 		historyArray[odIdx] = [];
 		od--;
 	}
@@ -114,13 +125,13 @@ let checkSudoku = function() {
 		// ブロックのための添え字
 		let currGroupRow = Math.floor(r/3); // 1～3行目は0、4～6行目は1、7～9行目は2
 		for (let c = 0; c < 9; c++) {
-			warp[r] += parseInt(data.rows[r].cols[c].disp);
-			weft[c] += parseInt(data.rows[r].cols[c].disp);
+			warp[r] += parseInt(disp[r*9+c]);
+			weft[c] += parseInt(disp[r*9+c]);
 
 			// ブロックのための添え字
 			let currGroupCol = Math.floor(c/3); // 1～3列目は0、4～6列目は1、7～9列目は2
 			let grup_pt = currGroupRow*3 + currGroupCol;
-			grup[grup_pt] += parseInt(data.rows[r].cols[c].disp);
+			grup[grup_pt] += parseInt(disp[r*9+c]);
 		}
 	}
 	// それぞれの数値を全部足すと45になることを確認
@@ -132,33 +143,62 @@ let checkSudoku = function() {
 		console.log("check is NG!!!");
 	}
 }
+//問題に穴を空ける
+let digSudoku = function() {
+
+	console.log("IN digSudoku");
+}
+
 
 let app = new Vue({
 	el: '#tbl',
-	data:{table : data
+	data:{disp : disp,
+		onmouse: isOnmouse,
+		samenumber: isSamenumber,
+		relatedblock: isRelatedblock,
 	},
 	created: function(){
 		console.log("created");
+//		// 問題の完成形を作成
+//		let od = 1;
+//		let id = setInterval(function () {
+//			od = makeSudoku(od);
+//			if (od < 1 || od > 81) {
+//	            // タイマー停止
+//	            clearInterval(id);
+//	            // チェック
+//	    		checkSudoku();
+//	    		// 問題に穴を空ける
+//	    		digSudoku();
+//			}
+//		}, 1);
 
-		// 問題の完成形を作成
-		let od = 1;
-		let id = setInterval(function () {
-			od = makeSudoku(od);
-			if (od < 1 || od > 81) {
-	            // タイマー停止
-	            clearInterval(id);
-	            // チェック
-	    		checkSudoku();
+	},
+	computed: {
+		computeddisp: function () {
+			console.log('IN computed');
+
+			// 問題の完成形を作成
+			let od = 1;
+			while (od >0 && od <= 81) {
+				od = makeSudoku(od);
 			}
-		}, 1);
+            // チェック
+    		checkSudoku();
+    		// 問題に穴を空ける
+    		digSudoku();
 
+			return this.disp;
+	    }
 	},
 	update: function(){
 		console.log("update");
 	},
 	methods: {
 		mouseover: function (r, c) {
-			this.table.rows[r].cols[c].onmouse = true;
+			let idx = r*9+c;
+			console.log("IN mouseover("+r+", "+c+") index="+idx);
+			this.onmouse[idx] = true;
 
 			// グループの添え字
 			let currGroupRow = Math.floor(r/3); // 1～3行目は0、4～6行目は1、7～9行目は2
@@ -166,6 +206,7 @@ let app = new Vue({
 
 			for (let cr = 0; cr < 9; cr++) {
 				for (let cc = 0; cc < 9; cc++) {
+					let gidx = cr*9+cc;
 
 					// グループの添え字
 					let chkGroupRow = Math.floor(cr/3); // 1～3行目は0、4～6行目は1、7～9行目は2
@@ -173,24 +214,26 @@ let app = new Vue({
 
 					if (currGroupRow == chkGroupRow && currGroupCol == chkGroupCol) {
 						// 同じグループ
-						this.table.rows[cr].cols[cc].relatedblock = true;
+						this.relatedblock[gidx] = true;
 					}
 
 					if (r == cr || c == cc) {
 						// 縦または横が同じ
-						this.table.rows[cr].cols[cc].relatedblock = true;
+						this.relatedblock[gidx] = true;
 					}
 
-					if (this.table.rows[r].cols[c].disp == this.table.rows[cr].cols[cc].disp) {
+					if (this.disp[idx] == this.disp[gidx]) {
 						// 中身の数字が同じ
-						this.table.rows[cr].cols[cc].samenumber = true;
+						this.samenumber[gidx] = true;
 					}
 				}
 			}
 
 		},
 		mouseleave: function (r, c) {
-			this.table.rows[r].cols[c].onmouse = false;
+			let idx = r*9+c;
+			console.log("IN mouseleave("+r+", "+c+") index="+idx);
+			this.onmouse[idx] = true;
 
 			// グループの添え字
 			let currGroupRow = Math.floor(r/3); // 1～3行目は0、4～6行目は1、7～9行目は2
@@ -198,6 +241,7 @@ let app = new Vue({
 
 			for (let cr = 0; cr < 9; cr++) {
 				for (let cc = 0; cc < 9; cc++) {
+					let gidx = cr*9+cc;
 
 					// グループの添え字
 					let chkGroupRow = Math.floor(cr/3); // 1～3行目は0、4～6行目は1、7～9行目は2
@@ -205,20 +249,21 @@ let app = new Vue({
 
 					if (currGroupRow == chkGroupRow && currGroupCol == chkGroupCol) {
 						// 同じグループ
-						this.table.rows[cr].cols[cc].relatedblock = false;
+						this.relatedblock[gidx] = false;
 					}
 
 					if (r == cr || c == cc) {
 						// 縦または横が同じ
-						this.table.rows[cr].cols[cc].relatedblock = false;
+						this.relatedblock[gidx] = false;
 					}
 
-					if (this.table.rows[r].cols[c].disp == this.table.rows[cr].cols[cc].disp) {
+					if (this.disp[idx] == this.disp[gidx]) {
 						// 中身の数字が同じ
-						this.table.rows[cr].cols[cc].samenumber = false;
+						this.samenumber[gidx] = false;
 					}
 				}
 			}
+
 		}
 	}
 })
